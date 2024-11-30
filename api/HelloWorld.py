d07dd82da9290d88bc5ddc6e04decc6a052a26e7
cc1287 <cc1287@yeah.net>
2024-11-30 14:39:23 +0800

    Update

--- api/HelloWorld.py
+++ api/HelloWorld.py
@@ -1,4 +1,5 @@
 from flask import *
+import requests
 
 app = Flask(__name__)
 
@@ -9,4 +10,10 @@ def root():
     return 'Hello World!'
 
 
-# app.run()
+@app.route('/bilibili_search')
+def bilibili_search():
+    page = request.args.get('page')
+    keyword = request.args.get('keyword')
+    res = requests.get("https://api.bilibili.com/x/web-interface/search/all/v2?page={}&keyword={}"
+                       .format(page, keyword))
+    return jsonify(res)
